7438f3ef3dd39610a4934cc311d79e836439e022
Hugo Ruíz <7987506+hugotown@users.noreply.github.com>
2025-10-16 09:19:29 -0600

    Base nix w/ dot files

---
+++ nixos/flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "hugotown nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+          pkgs.alacritty
+          pkgs.fastfetch
+          pkgs.gh
+          pkgs.neovim
+          pkgs.vim
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      programs.bash.enable = true;
+
+      users.users.hugoruiz.shell = pkgs.bash;
+      
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mp-i9-16i
+    darwinConfigurations."mp-i9-16i" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."mp-i9-16i".pkgs;
+
+  };
+}
