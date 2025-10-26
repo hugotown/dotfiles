48bb4cd104d3eb506ce6895af88e86f4e1ca6eb7
Hugo Ruíz <7987506+hugotown@users.noreply.github.com>
2025-10-26 15:32:57 -0600

    fixed fcitx5-qt6

--- nixos/hosts/nixos/lenovo-nixos-btw/default.nix
+++ nixos/hosts/nixos/lenovo-nixos-btw/default.nix
@@ -69,7 +69,8 @@
 
   # Input method support (fcitx5)
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
     ];
